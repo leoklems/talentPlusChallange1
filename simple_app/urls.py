987b4ba67746decbbf83965a63d22d1a8b008438
leoklems <leoklems@yahoo.com>
2022-11-15 03:30:09 +0100

    better documentation

--- simple_app/urls.py
+++ simple_app/urls.py
@@ -5,7 +5,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('', views.display_view),
-    # path('<int:id>/', views.drink_detail)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- simple_app/views.py
+++ simple_app/views.py
@@ -6,26 +6,26 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+
 # Create your views here.
 
 @api_view(['GET', 'POST'])
 def display_view(request, format=None):
     """
     :param request:
-        drinks: Get all the drinks
+        display: Get all the displays
         serializer: serialize them
-    :return: json
+    :return: renders a html page with display as json object
     """
     if request.method == 'GET':
-        drinks = Display.objects.all()
-        serializer = DisplaySerializer(drinks, many=True)
+        display = Display.objects.all()
+        serializer = DisplaySerializer(display, many=True)
         # return Response({"displays": serializer.data})
         print(serializer.data)
         return render(request, 'index.html', {"displays": serializer.data})
-        # return JsonResponse({"Drinks": serializer.data}, safe=False) # When runing the first time
 
-    if request.method == 'POST':
-        serializer = DisplaySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status= status.HTTP_201_CREATED)
+    # if request.method == 'POST':
+    #     serializer = DisplaySerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status= status.HTTP_201_CREATED)
